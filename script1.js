aa90b4c17da7cdca4bc18c62f24b57d946d3c293
Doniyor <doniyortilavov07@gmail.com>
2024-02-24 18:31:03 +0500

    js hw 6

---
+++ script1.js
@@ -0,0 +1,8 @@
+let box = +prompt("Istalgan soni kiriting")
+for (let i = 0; i < box;) {
+    i++
+    console.log(i + " qo'ylar...")
+}
+
+
+
---
+++ script2.js
@@ -0,0 +1,4 @@
+var name = prompt("Ismingizni kiriting")
+var a = +prompt("Hozirgi yilni kiriting")
+var b = +prompt("Tug'ilgan yilingizni kiriting")
+alert(name + ", " + "yoshingiz " + (a-b))
